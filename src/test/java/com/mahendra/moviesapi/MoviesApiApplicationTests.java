5752275878ad88ebc5307d0f63a535cbdf6b1b53
sthalathoty <sthalathoty@gmail.com>
2024-11-20 18:33:25 +0000

    Updated by s2thala  MoviesApiApplicationTests.java

--- src/test/java/com/mahendra/moviesapi/MoviesApiApplicationTests.java
+++ src/test/java/com/mahendra/moviesapi/MoviesApiApplicationTests.java
@@ -2,6 +2,9 @@ package com.mahendra.moviesapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.mahendra.LibraryApiApplication;
 
 @SpringBootTest
 class MoviesApiApplicationTests {
