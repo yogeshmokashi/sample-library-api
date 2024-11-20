857edb182453210738b26df2e998cdf6f528861f
selahattinejder1 <selahattinejder@hotmail.com>
2024-11-20 13:24:58 -0500

    Update MoviesApiApplicationTests.java

--- src/test/java/com/mahendra/moviesapi/MoviesApiApplicationTests.java
+++ src/test/java/com/mahendra/moviesapi/MoviesApiApplicationTests.java
@@ -1,13 +1,17 @@
 package com.mahendra.moviesapi;
-
+ 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ContextConfiguration;
+ 
+import com.mahendra.library.LibraryApiApplication;
+ 
 @SpringBootTest
+@ContextConfiguration(classes = LibraryApiApplication.class)
 class MoviesApiApplicationTests {
-
+ 
 	@Test
 	void contextLoads() {
 	}
-
+ 
 }
