ac9d85db137cd2236822e88750939f6169e122d6
acenbees <dhayanand@gmail.com>
2024-11-20 10:25:27 -0800

    Update MoviesApiApplicationTests.java
    Fix to test cases

--- src/test/java/com/mahendra/moviesapi/MoviesApiApplicationTests.java
+++ src/test/java/com/mahendra/moviesapi/MoviesApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.mahendra.moviesapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = LibraryApiApplication.class)
 class MoviesApiApplicationTests {
 
 	@Test
